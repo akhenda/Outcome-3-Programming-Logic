89e54acf2175136fbf78c07143238e18001956d1
Joseph Akhenda <akhenda@gmail.com>
2016-10-08 17:50:24 +0300

    Update fibonacci.py

--- fibonacci-gen/fibonacci.py
+++ fibonacci-gen/fibonacci.py
@@ -7,16 +7,3 @@ def fibonacci(n):
 		output.append(a)
 	return output
 print fibonacci(10)
-
-
-
-
-
-
-
-# def fibonacci(n):
-	# if n==0 or n==1 or n==2:
-		# return n
-	# return fibonacci(n-1) + fibonacci(n-2)
-
-# print fibonacci(10)
